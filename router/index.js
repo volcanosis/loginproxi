a3f00a5950b54dd224f4bb71502902f85559e568
SAudelOG <audel91@gmail.com>
2015-07-14 23:50:35 -0500

    move routes on each own secction

---
+++ router/index.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+  app.get('/', function(req, res){
+    res.render('home',{
+      title:'Welcome to Volcanosis Home!',
+      organization:'Volcanosis'
+    });
+  });
+
+  app.post('/CreateApp', function(req, res){
+    console.log(req.body);
+    return res.redirect(303, '/');
+  });
+};
