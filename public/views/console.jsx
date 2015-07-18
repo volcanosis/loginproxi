a818fcde682e5ec39e1fc435aa310a655692c661
SAudelOG <audel91@gmail.com>
2015-07-18 14:41:54 -0500

    change order

--- public/views/console.jsx
+++ public/views/console.jsx
@@ -3,17 +3,6 @@
 var Layout = require('./layout.jsx');
 var React = require('react');
 
-module.exports = React.createClass({
-  render:function render(){
-    return(
-      <Layout {...this.props}>
-        <div id="container">
-          <CreateAppBox />
-        </div>
-      </Layout>
-    );
-  }
-});
 var CreateAppBox = React.createClass({
   render: function(){
     return(
@@ -24,6 +13,18 @@ var CreateAppBox = React.createClass({
           <button className="btnCreateApp" type="submit">Create App</button>
         </form>
       </div>
+    );
+  }
+});
+
+module.exports = React.createClass({
+  render:function render(){
+    return(
+      <Layout {...this.props}>
+        <div id="container">
+          <CreateAppBox />
+        </div>
+      </Layout>
     );
   }
 });
