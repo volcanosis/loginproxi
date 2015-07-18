a902e5479decc4fadad341507501c2707f822ece
SAudelOG <audel91@gmail.com>
2015-07-18 13:56:50 -0500

    data saved on MongoLab

--- router/index.js
+++ router/index.js
@@ -1,8 +1,27 @@
 /*
  *Router
  */
+var credentials = require('../credentials.js');
+//database configuration
+var mongoose = require('mongoose');
+var options = {
+  server:{
+    socketOptions:{keepAlive:1}
+  }
+};
+
+var Application = require('../models/application.js');
 
 module.exports = function(app){
+
+  switch (app.get('env')) {
+    case 'development':
+      mongoose.connect(credentials.mongo.dev.connectionString, options);
+      break;
+    default:
+      throw new Error('Unknown exectios enviroment: ' + app.get('env'));
+  }
+
   app.get('/', function(req, res){
     res.render('home',{
       title:'Welcome to Volcanosis Home!',
@@ -11,7 +30,17 @@ module.exports = function(app){
   });
 
   app.post('/CreateApp', function(req, res){
-    console.log(req.body);
+    var appName = req.body.appName,
+        domain = req.body.appDomain;
+
+    new Application({
+      appName: appName,
+      domain: domain,
+      isActive: true,
+    }).save(function(err){
+      if(err) return console.log('Database connection err');
+      console.log('Data saved');
+    });
     return res.redirect(303, '/');
   });
 };
