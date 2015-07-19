43d9f56c1f1d3e708e0ceeaa99c65deffa338bd0
SAudelOG <audel91@gmail.com>
2015-07-18 19:40:55 -0500

    jquery package added and required

--- public/loginproxy.js
+++ public/loginproxy.js
@@ -5,6 +5,8 @@ var Client = require('react-engine/lib/client');
 //include all view files
 require('./views/**/*.jsx', {glob:true});
 
+var $ = require('jquery');
+
 //boot
 var options = {
   //function that can be called
