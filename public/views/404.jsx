a4fc8a40099ae759d2d3b1ce32aece1ea36071e2
SAudelOG <audel91@gmail.com>
2015-08-23 18:39:05 -0500

    style on 404 err

--- public/views/404.jsx
+++ public/views/404.jsx
@@ -3,13 +3,28 @@
 var Layout = require('./layout.jsx');
 var React = require('react');
 
+//inline style
+var h3Stlye = {
+  color: '#555',
+  fontSize: 22,
+  fontWeight: 400
+}
+var layoutStyle = {
+  color: '#888',
+  display: 'table',
+  fontFamily: 'sans-serif',
+  height: '100%',
+  textAlign: 'center',
+  width: '100%'
+}
+
 module.exports = React.createClass({
 
   render: function render() {
 
     return (
-      <Layout {...this.props}>
-        <h3>URL: {this.props.url} - Not Found(404)</h3>
+      <Layout {...this.props} style={layoutStyle}>
+        <h3 style={h3Stlye}>URL: {this.props.url} - Not Found(404)</h3>
       </Layout>
     );
   }
