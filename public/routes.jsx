9285048e2a53b9b68472375fac54520445fb25e8
SAudelOG <audel91@gmail.com>
2015-08-23 01:39:17 -0500

    remove dataview

--- public/routes.jsx
+++ public/routes.jsx
@@ -5,10 +5,9 @@ var Router = require('react-router');
 
 var Loginproxy = require('./views/loginproxy.jsx')
 var Console = require('./views/console.jsx')
-var Data = require('./views/data.jsx')
 
 var routes = (
-  <Router.Route handler = {Loginproxy}>
+  <Router.Route path = "/" handler = {Loginproxy}>
     <Router.Route path = "console" handler = {Console} />
   </Router.Route>
 );
