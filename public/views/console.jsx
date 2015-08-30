f54f464b93a85015ca110de7f133fc909ff71017
SAudelOG <audel91@gmail.com>
2015-08-30 18:10:58 -0500

    create application form now with mdl design

--- public/views/console.jsx
+++ public/views/console.jsx
@@ -232,27 +232,33 @@ var Api = React.createClass({
 var APISSelected = []; //to save the hidden input field
 //React componet to create new application
 var CreateAppForm = React.createClass({
+  logState:function(){
+    console.log(this.state);
+  },
   handleSubmit: function(evt){
     evt.preventDefault();
-    var Apis = _.map(APISSelected, function(Api){
+    //convert the apis selected array into object
+    var Apis = _.map(this.state.APISSelected, function(Api){
       return{
         ApiID: Api
       }
     })
+    //create the body object with user inputs
     var body = {
       appName: React.findDOMNode(this.refs.appName).value.trim(),
       appDomain: React.findDOMNode(this.refs.appDomain).value.trim(),
       apis: Apis
     }
-    console.log(this.state);
-    if(!body.appName || !body.appDomain || !body.apis) return;
 
-    //send data and url to ConsoleBox commponent and create application
+    //TODO: validate and show warning message on missing fields
+    if(!body.appName || !body.appDomain || _.isEmpty(body.apis)) return;
+
+    //send data and url to ConsoleBox commponent, create application
     //and reload application list because the component owns state
     var url = this.props.createAppUrl;
     this.props.onCreateAppSubmit(body, url);
 
-    //clean inputs after submit
+    //finally clean inputs after submit
     React.findDOMNode(this.refs.appName).value = '';
     React.findDOMNode(this.refs.appDomain).value = '';
     return;
@@ -281,11 +287,23 @@ var CreateAppForm = React.createClass({
   componentDidMount: function(){
     this.fetchAPISFromServer();
   },
-  logChange: function(val){
-    APISSelected = val.split(',')
-  },
   getInitialState: function(){
-    return {options:[]};
+    return {
+      options:[], //for existents apis
+      APISSelected:[] //for apis that user select
+    };
+  },
+  //this function are triggered each time the user
+  //select and api From the input
+  logChange: function(val){
+    //if there is no value on hidden input
+    //set the state as empty array
+    if (!val){
+      this.state.APISSelected = [];
+    } else{
+    //set the value of hidden field on state
+     this.state.APISSelected = val.split(',');
+    }
   },
   render: function(){
     var Select = require('react-select');
@@ -294,18 +312,24 @@ var CreateAppForm = React.createClass({
         <div className="mdl-card__supporting-text">
           <h4>Create new application</h4>
         <form name="crateAppForm" onSubmit={this.handleSubmit}>
-          <input
-            ref="appName"
-            id="appNameField"
-            type="text"
-            placeholder="App name"
-          />
-          <input
-            ref="appDomain"
-            id="appDomainField"
-            type="text"
-            placeholder="Domain name"
-          />
+          <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input
+              className="mdl-textfield__input"
+              type="text"
+              id="appNameField"
+              ref="appName"
+              />
+            <label className="mdl-textfield__label"  htmlFor="appNameField">App name</label>
+          </div>
+          <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input
+              className="mdl-textfield__input"
+              type="text"
+              id="appDomainField"
+              ref="appDomain"
+              />
+            <label className="mdl-textfield__label" htmlFor="appDomainField">Domain name</label>
+          </div>
           <Select
               name="apiList"
               multi={true}
@@ -313,7 +337,11 @@ var CreateAppForm = React.createClass({
               onChange={this.logChange}
               placeholder = "Select APIS to use on your application"
             />
-          <button value="Post" className="btnCreateApp" type="submit">Create App</button>
+          <div className="mdl-card__actions mdl-card--border">
+            <button value="Post" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"  type="submit">
+                Create
+            </button>
+          </div>
         </form>
         </div>
       </div>
