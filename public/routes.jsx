00724617c5b240835b76fdb8e2313ddbd5c26bc7
SAudelOG <audel91@gmail.com>
2015-08-30 01:34:30 -0500

    team view added to react router

--- public/routes.jsx
+++ public/routes.jsx
@@ -3,12 +3,14 @@
 var React = require('react');
 var Router = require('react-router');
 
-var Loginproxy = require('./views/loginproxy.jsx')
-var Console = require('./views/console.jsx')
+var Loginproxy = require('./views/loginproxy.jsx');
+var Console = require('./views/console.jsx');
+var Team = require('./views/team.jsx');
 
 var routes = (
   <Router.Route path = "/" handler = {Loginproxy}>
     <Router.Route path = "console" handler = {Console} />
+    <Router.Route path = "team" handler = {Team} />
   </Router.Route>
 );
 
