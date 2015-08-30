271ef3281517352aa30e253bc713339f9eac52b0
SAudelOG <audel91@gmail.com>
2015-08-29 22:59:14 -0500

    mdl layout implemented

--- public/views/layout.jsx
+++ public/views/layout.jsx
@@ -4,14 +4,13 @@ var React = require('react');
 
 module.exports = React.createClass({
   render:function render(){
-    var mdClass = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent ";
     return(
       <html>
         <head>
           <meta charSet='utf-8' />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="stylesheet" type="text/css" href="/css/vendor/react-select/default.css"/>
-          <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.orange-indigo.min.css"/>
+          <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.red-indigo.min.css"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 
           <link rel="stylesheet" type="text/css" href="/css/app.css"/>
@@ -20,8 +19,37 @@ module.exports = React.createClass({
           </title>
         </head>
         <body style={this.props.style}>
-          <h1>Volcanosis developer console</h1>
-          {this.props.children}
+          {/*The drawer is always open in large screens. The header is always shown,even in small screens. */}
+          <div className="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+            <header className="mdl-layout__header">
+              <div className="mdl-layout__header-row">
+                <span className="mdl-layout-title">Volcanosis console</span>
+                <div className="mdl-layout-spacer"></div>
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                  <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="fixed-header-drawer-exp">
+                    <i className="material-icons">search</i>
+                  </label>
+                  <div className="mdl-textfield__expandable-holder">
+                    <input className="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp" />
+                  </div>
+                </div>
+              </div>
+            </header>
+            <div className="mdl-layout__drawer">              
+              <nav className="mdl-navigation">
+                <a className="mdl-navigation__link" href="">Link</a>
+                <a className="mdl-navigation__link" href="">Link</a>
+                <a className="mdl-navigation__link" href="">Link</a>
+                <a className="mdl-navigation__link" href="">Link</a>
+              </nav>
+            </div>
+            <main className="mdl-layout__content">
+              <div className="page-content">
+                {/*content goes here */}
+                {this.props.children}
+              </div>
+            </main>
+          </div>
         </body>
         <script src="/bundle.js"></script>
         <script src="https://storage.googleapis.com/code.getmdl.io/1.0.4/material.min.js"></script>
