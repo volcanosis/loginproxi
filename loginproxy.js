3c41e46353e348e955a615d8629ab416c00fb7d9
SAudelOG <audel91@gmail.com>
2015-09-27 00:54:55 -0500

    just ident

--- loginproxy.js
+++ loginproxy.js
@@ -20,27 +20,27 @@ app.engine( 'jsx', engine );
 app.set( 'views', path.join( __dirname, '/public/views' ) );
 app.set( 'view engine', 'jsx' );
 
-app.set('view', renderer.expressView );
+app.set( 'view' , renderer.expressView );
 
 //public assets
 app.use( express.static( path.join( __dirname, '/public' ) ) );
 
-app.set('port', process.env.PORT || 3000);
+app.set( 'port', process.env.PORT || 3000 );
 
-app.use(require('body-parser').json());
-app.use(require('body-parser').urlencoded({
-  extended:true
+app.use( require( 'body-parser' ).json() );
+app.use( require( 'body-parser' ).urlencoded({
+  extended : true
 }));
 
 //router
-(require('./router')(app));
+(require( './router' )( app ) );
 
-http.createServer(app).listen(app.get('port'),function(){
+http.createServer( app ).listen( app.get( 'port' ), function(){
 
   var host = 'localhost';
 
-  var port = app.get('port');
+  var port = app.get( 'port' );
 
-  console.log('Express started in: ' + app.get('env') +
+  console.log( 'Express started in: ' + app.get( 'env' ) +
   ' mode on http://%s:%s; press Ctrl-c to terminate', host, port);
 });
